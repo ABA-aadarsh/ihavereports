cf0d8ffd7e07af99e9b5ce2325ec3dfc195ad548
Aadarsh <aadarshbandhuaryal@gmail.com>
2024-05-11 15:43:20 +0545

    try to implement ai for algorithm

---
+++ ai.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY);
+
+async function ai_response(prompt) {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+const question="Write a c program to print 'Hello World meow meow' 10 times using for loop"
+ai_response(`Write a algorithm for this question '${question}'. Give only steps. Each step should be minimal in the sense that it is not lengthy`);
