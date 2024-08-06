c0cc1970d7cbf858ffa9f3104fcff89f7520c76f
Aadarsh <aadarshbandhuaryal@gmail.com>
2024-08-06 21:34:31 +0545

    foldername space issue

--- index.js
+++ index.js
@@ -1,88 +1,93 @@
-const { exit } = require("process")
-const {  allowedExtensions , CommandExec} = require("./src/commandExecutor.js")
-const { checkFolderExists } = require("./src/lib.js")
-const { createDocData } = require("./src/docGenerator.js")
-const fs = require("fs")
-const path = require("path")
-const { Packer } = require("docx")
-const {rl}=require("./src/readLineHandler.js")
+const { exit } = require("process");
+const { allowedExtensions, CommandExec } = require("./src/commandExecutor.js");
+const { checkFolderExists } = require("./src/lib.js");
+const { createDocData } = require("./src/docGenerator.js");
+const fs = require("fs");
+const path = require("path");
+const { Packer } = require("docx");
+const { rl } = require("./src/readLineHandler.js");
 
 function getUserInput(question) {
     return new Promise((resolve) => {
-      rl.question(question, (answer) => {
-        resolve(answer);
-      });
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
     });
 }
 
-
 const main = async () => {
-    console.log(":) ihavereports\n\n")
-    let codeFolderPath ;
-    const argv = process.argv
+    console.log(":) ihavereports\n\n");
+    let codeFolderPath;
+    const argv = process.argv;
     if (argv.length < 3) {
-        codeFolderPath = await getUserInput("Enter the folder path where your codes are saved \n(if this progam exist in the same folder just press enter): ")
-        codeFolderPath == '\n' ? codeFolderPath="." : null; 
-    }else{
-        codeFolderPath = argv[2] //TODO: Verify
+        codeFolderPath = await getUserInput(
+            "Enter the folder path where your codes are saved \n(if this progam exist in the same folder just press enter): ",
+        );
+        codeFolderPath == "\n" ? (codeFolderPath = ".") : null;
+    } else {
+        codeFolderPath = argv[2]; //TODO: Verify
     }
 
-    const folderStatus = checkFolderExists(codeFolderPath)
+    const folderStatus = checkFolderExists(codeFolderPath);
     if (!folderStatus) {
-        console.log("Error in accessing folder\nProbably the directory does not exists")
-        exit(1)
+        console.log(
+            "Error in accessing folder\nProbably the directory does not exists",
+        );
+        exit(1);
     }
+    let author = await getUserInput(
+        "\nEnter author name (this would be shown in output)\n(user) ",
+    );
+    if (author == "\n") author = "user";
+    console.log(`Set ${author} as author\n\n`);
 
-    let author = await getUserInput("\nEnter author name (this would be shown in output)\n(user) ")
-    if(author=='\n') author = "user"
-    console.log(`Set ${author} as author\n\n`)
+    const executor = new CommandExec(codeFolderPath);
 
-    const executor = new CommandExec(codeFolderPath)
-
-    const codeFiles = fs.readdirSync(
-        path.resolve(codeFolderPath)
-    ).filter(i => allowedExtensions.includes(i.split(".")[1])).map(i => (
-        {
+    const codeFiles = fs
+        .readdirSync(path.resolve(codeFolderPath))
+        .filter((i) => allowedExtensions.includes(i.split(".")[1]))
+        .map((i) => ({
             path: path.join(codeFolderPath, i),
             name: i.split(".")[0],
             ext: i.split(".")[1],
             question: null,
             consoleInterface: null,
-            code: null
-        }
-    ))
+            code: null,
+        }))
+        .sort((a, b) => {
+            return Number(a.name) - Number(b.name);
+        });
 
-    if(codeFiles.length==0){
-        console.log("It seems there is no codes files inside. Exiting\n")
-        exit(0)
+    if (codeFiles.length == 0) {
+        console.log("It seems there is no codes files inside. Exiting\n");
+        exit(0);
     }
 
-    executor.setCodeFiles(
-        codeFiles
-    )
+    executor.setCodeFiles(codeFiles);
 
-    executor.modifyCode()
+    executor.modifyCode();
 
-    executor.compileAll()
+    executor.compileAll();
 
-    await executor.executeAll()
-    // console.clear()
-    console.log("-----------\nPrograms execution done. \n\nNow collecting data...\n")
-    const data = executor.collectData()
-    console.log("Done\n")
-    executor.clearTemps()
-    const docData = await createDocData(data, author)
+    await executor.executeAll();
+    console.log(
+        "-----------\nPrograms execution done. \n\nNow collecting data...\n",
+    );
+    const data = executor.collectData();
+    console.log("Done\n");
+    executor.clearTemps();
+    const docData = await createDocData(data, author);
     Packer.toBuffer(docData).then(async (buffer) => {
-        fs.writeFileSync(path.join(codeFolderPath,"Report.docx"), buffer);
+        fs.writeFileSync(path.join(codeFolderPath, "Report.docx"), buffer);
         console.log(`Report.docx is created at path ${codeFolderPath}`);
-        console.log("\nSource code available at https://github.com/ABA-aadarsh/ihavereports\n\n");
+        console.log(
+            "\nSource code available at https://github.com/ABA-aadarsh/ihavereports\n\n",
+        );
 
-        await getUserInput("Press enter to exit: ")
-        rl.close()
-        exit(0)
+        await getUserInput("Press enter to exit: ");
+        rl.close();
+        exit(0);
     });
-}
+};
 
-
-
-main()
+main();
--- src/commandExecutor.js
+++ src/commandExecutor.js
@@ -1,129 +1,124 @@
-const path = require("path")
-const fs = require("fs")
-const {spawn, execSync}=require("child_process")
-const {createEmptyFolder, clearExecutables} = require("./lib.js")
-const {rl} = require("./readLineHandler.js")
+const fs = require("fs");
+const { spawn, execSync } = require("child_process");
+const { createEmptyFolder, clearExecutables } = require("./lib.js");
+const { rl } = require("./readLineHandler.js");
 
 const os = {
-    linux : "linux",
-    windows: "win32"
-}
-const allowedExtensions = ["cpp","c"]
+    linux: "linux",
+    windows: "win32",
+};
+const allowedExtensions = ["cpp", "c"];
 
 class CommandExec {
-    constructor(folderPath, list =[]){
-        this.folderPath = folderPath
+    constructor(folderPath, list = []) {
+        this.folderPath = folderPath;
 
-        if(process.platform==os.linux){
-            this.machine = os.linux
-        }else if(process.platform==os.windows){
-            this.machine=os.windows
-        }
-        else{
-            throw Error("Seems application can't run on this machine")
+        if (process.platform == os.linux) {
+            this.machine = os.linux;
+        } else if (process.platform == os.windows) {
+            this.machine = os.windows;
+        } else {
+            throw Error("Seems application can't run on this machine");
         }
 
         this.folderNames = {
             consoleOutput: "consoleInterface",
-            modifiedCodes: "modifiedCodes"
-        }
+            modifiedCodes: "modifiedCodes",
+        };
 
-        this.codesFiles = list.filter(i=>allowedExtensions.includes(i?.ext || null))
+        this.codesFiles = list.filter((i) =>
+            allowedExtensions.includes(i?.ext || null),
+        );
 
-
-        createEmptyFolder(this.folderPath, this.folderNames.modifiedCodes)
-        createEmptyFolder(this.folderPath, this.folderNames.consoleOutput)
-        clearExecutables(this.folderPath)
+        createEmptyFolder(this.folderPath, this.folderNames.modifiedCodes);
+        createEmptyFolder(this.folderPath, this.folderNames.consoleOutput);
+        clearExecutables(this.folderPath);
     }
 
-    setCodeFiles( list = []){
-        this.codesFiles = list.filter(i=>allowedExtensions.includes(i.ext))
+    setCodeFiles(list = []) {
+        this.codesFiles = list.filter((i) => allowedExtensions.includes(i.ext));
     }
-    getCompileCommand (ext, fullpath, fileName, exePaths){
-        const compiler = ext == "c" ? "gcc" : "g++"
-        const outputType = this.machine == os.linux ? "o" : "exe"
-        return `${compiler} ${fullpath} -o ${path.resolve(exePaths, fileName)}.${outputType}`
+    getCompileCommand(ext, fullpath, fileName, exePaths) {
+        const outputType = this.machine == os.linux ? "o" : "exe";
+        return `${ext == "c" ? "gcc" : "g++"} ${require("path").resolve(fullpath).replace(/ /g, `\\ `)} -o ${require("path").resolve(exePaths, fileName).replace(/ /g, `\\ `)}.${outputType}`;
     }
-    async executeAll(){
-        const executePromiseSeries=async (iterable,action)=>{
-            try{
+    async executeAll() {
+        const executePromiseSeries = async (iterable, action) => {
+            try {
                 for (const x of iterable) {
-                        console.log("Executing Program : ", x);
-                        await action(x)
-                        console.log("\n")
+                    console.log("Executing Program : ", x);
+                    await action(x);
+                    console.log("\n");
                 }
-                return true
-            }catch(err){
-                console.log(err)
-                return false
+                return true;
+            } catch (err) {
+                console.log(err);
+                return false;
             }
-        }
-        const executables=fs.readdirSync(
-            path.resolve(this.folderPath)
-        ).filter(i=>{
-            if(this.machine==os.linux){
-                return i.split(".")[1]=="o"
-            }else if(this.machine==os.windows){
-                return i.split(".")[1]=="exe"
-            }else{
-                return false
-            }
-        })
+        };
+        const executables = this.codesFiles.map((i) => {
+            return i.name + (this.machine == os.linux ? ".o" : ".exe");
+        });
 
-        const executePromise=(e)=>new Promise((resolve,reject)=>{
+        const executePromise = (e) =>
+            new Promise((resolve) => {
                 const programProcess = spawn(
-                    path.resolve(this.folderPath, e),
-                    { 
-                        stdio: ['pipe', 'pipe', 'pipe'], 
-                        detached:false,
-                        cwd: path.resolve(this.folderPath)
-                    }
+                    require("path").resolve(this.folderPath, e),
+                    {
+                        stdio: ["pipe", "pipe", "pipe"],
+                        detached: false,
+                        cwd: require("path").resolve(this.folderPath),
+                    },
                 );
                 const outputFileStream = fs.createWriteStream(
-                    path.resolve(this.folderPath, this.folderNames.consoleOutput, `${e.split(".")[0]}.txt`)
-                )
-                programProcess.stdout.on('data', (data) => {
+                    require("path").resolve(
+                        this.folderPath,
+                        this.folderNames.consoleOutput,
+                        `${e.split(".")[0]}.txt`,
+                    ),
+                );
+                programProcess.stdout.on("data", (data) => {
                     console.log(`${data}`);
                     outputFileStream.write(data);
                 });
-                rl.on('line', (input) => {
-                    programProcess.stdin.write(input + '\n');
-                    outputFileStream.write(input+"\n")
+                rl.on("line", (input) => {
+                    programProcess.stdin.write(input + "\n");
+                    outputFileStream.write(input + "\n");
                 });
-    
-                programProcess.on('close', (code) => {
-                    console.log('Program execution completed.\n Execution code = ', code);
+
+                programProcess.on("close", (code) => {
+                    console.log(
+                        "Program execution completed.\n Execution code = ",
+                        code,
+                    );
                     outputFileStream.end();
-                    resolve()
+                    resolve();
                 });
-            }
-        )
-        await executePromiseSeries(executables,executePromise)
+            });
+        await executePromiseSeries(executables, executePromise);
     }
-    compile({fileName, sourcepath, executablePath, ext}){
+    compile({ fileName, sourcepath, executablePath, ext }) {
         const command = this.getCompileCommand(
-            ext, 
-            sourcepath, 
-            fileName, 
-            executablePath
-        )
-        execSync(command)
-        return true
+            ext,
+            sourcepath,
+            fileName,
+            executablePath,
+        );
+        execSync(command);
+        return true;
     }
-    compileAll (){
-        for(let i = 0; i<this.codesFiles.length ; i++){
-            const _ = this.codesFiles[i]
-            this.compile(
-                {
-                    fileName: _.name,
-                    sourcepath: _.path,
-                    executablePath: this.folderPath,
-                    ext: _.ext
-                }
-            )
+    compileAll() {
+        for (let i = 0; i < this.codesFiles.length; i++) {
+            const _ = this.codesFiles[i];
+            this.compile({
+                fileName: _.name,
+                sourcepath: _.path,
+                executablePath: this.folderPath,
+                ext: _.ext,
+            });
         }
     }
-    modifyCode(){
+    modifyCode() {
         function addFlushToPrintf(code) {
             const pattern = /printf\s*\(([^)]+)\);/g;
             return code.replace(pattern, (match, printfArgs) => {
@@ -131,63 +126,78 @@ class CommandExec {
             });
         }
 
-        const targetedFiles = this.codesFiles.filter(i=>i.ext=="c")
+        const targetedFiles = this.codesFiles.filter((i) => i.ext == "c");
 
-        targetedFiles.forEach(async(file, _)=>{
-            const fileFullName = file.name + "." + file.ext
-            const a=fs.readFileSync(
-                file.path
-                , "utf-8"
-            )
-            const modified=addFlushToPrintf(a);
-            const f=fs.writeFileSync(
-                path.resolve(this.folderPath, this.folderNames.modifiedCodes, fileFullName)    
-                ,modified
-            )
-            targetedFiles[_].path = path.resolve(this.folderPath, this.folderNames.modifiedCodes, fileFullName)
-        })
-        return true
+        targetedFiles.forEach(async (file, _) => {
+            const fileFullName = file.name + "." + file.ext;
+            const a = fs.readFileSync(file.path, "utf-8");
+            const modified = addFlushToPrintf(a);
+            targetedFiles[_].path = require("path")
+                .resolve(
+                    this.folderPath,
+                    this.folderNames.modifiedCodes,
+                    fileFullName,
+                )
+                .replace(/ /g, `\\ `);
+        });
+        return true;
     }
 
-
-    collectData(){
+    collectData() {
         // collect console interface and questions
-        const consoleBasePath = path.join(this.folderPath, this.folderNames.consoleOutput)
+        const consoleBasePath = require("path").join(
+            this.folderPath,
+            this.folderNames.consoleOutput,
+        );
         const questionRegex = /#question\(([\s\S]*?)\)/;
-        const clearRegex= /\/\*[\s\S]*?\*\//g;
+        const clearRegex = /\/\*[\s\S]*?\*\//g;
         let question;
-        this.codesFiles.forEach((i, index)=>{
+        this.codesFiles.forEach((i, index) => {
             const consoleData = fs.readFileSync(
-                path.join(consoleBasePath, `${i.name}.txt`),
-                "utf-8" 
-            )
+                require("path").join(consoleBasePath, `${i.name}.txt`),
+                "utf-8",
+            );
             const codeData = fs.readFileSync(
-                path.join(this.folderPath, `${i.name}.${i.ext}`),
-                "utf-8"
-            )
-            
-            const questionMatch = codeData.match(questionRegex) 
-            if(questionMatch && questionMatch[1]){
-                question = questionMatch[1].split("\n").map(line => line.trim()).join("\n")
-            }else{
-                question = "__ Write Question Here __"
+                require("path").join(this.folderPath, `${i.name}.${i.ext}`),
+                "utf-8",
+            );
+
+            const questionMatch = codeData.match(questionRegex);
+            if (questionMatch && questionMatch[1]) {
+                question = questionMatch[1]
+                    .split("\n")
+                    .map((line) => line.trim())
+                    .join("\n");
+            } else {
+                question = "__ Write Question Here __";
             }
-            this.codesFiles[index].code = codeData.replace(clearRegex,"").trimStart()
-            this.codesFiles[index].consoleInterface = consoleData
-            this.codesFiles[index].question = question
-        })
-        return this.codesFiles
+            this.codesFiles[index].code = codeData
+                .replace(clearRegex, "")
+                .trimStart();
+            this.codesFiles[index].consoleInterface = consoleData;
+            this.codesFiles[index].question = question;
+        });
+        return this.codesFiles;
     }
-    clearTemps(){
-        createEmptyFolder(this.folderPath, this.folderNames.modifiedCodes)
-        createEmptyFolder(this.folderPath, this.folderNames.consoleOutput)
-        fs.rmdirSync(path.join(this.folderPath, this.folderNames.modifiedCodes))
-        fs.rmdirSync(path.join(this.folderPath, this.folderNames.consoleOutput))
+    clearTemps() {
+        createEmptyFolder(this.folderPath, this.folderNames.modifiedCodes);
+        createEmptyFolder(this.folderPath, this.folderNames.consoleOutput);
+        fs.rmdirSync(
+            require("path").join(
+                this.folderPath,
+                this.folderNames.modifiedCodes,
+            ),
+        );
+        fs.rmdirSync(
+            require("path").join(
+                this.folderPath,
+                this.folderNames.consoleOutput,
+            ),
+        );
     }
 }
 
-module.exports ={
+module.exports = {
     CommandExec,
-    allowedExtensions
-}
-
+    allowedExtensions,
+};
